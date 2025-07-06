d156c9a1431daf845d184f02672394a0bfed983f
alexa neff <alexanef@buffalo.edu>
2025-07-06 14:43:54 -0400

    added frontend logini

---
+++ chapter3/src/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import path, {dirname} from 'path'
+import { fileURLToPath} from 'url'
+
+const app = express()
+const PORT = process.env.PORT || 5000 //checks if port environment variable is set, otherwise defaults to 5000
+
+
+//get filepath from url of curr module
+const __filename = fileURLToPath(import.meta.url)
+//get directory name from filepath
+const __dirname = dirname(__filename)
+
+//tell it exactly where public directory is
+//tell express to serve all files from public folder as static files
+app.use(express.static(path.join(__dirname, '../public')))
+
+//endpoint for serving html file from pulic directory
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))  //join current directory with pulic folder
+})
+
+
+
+
+
+
+
+
+
+//always have this at bottom of code
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
